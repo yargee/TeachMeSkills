492b8a968ed84b64bbf33ad28a25de0f8a8389c6
Pashinov Petr <yargee@bk.ru>
2023-10-14 00:01:57 +0300

    +

--- Homework_5(StringBuilder)/JokeViewer.cs
+++ Homework_5(StringBuilder)/JokeViewer.cs
@@ -1,19 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace JokesParser
+﻿namespace JokesParser
 {
     internal class JokesViewer
     {
         public static void View(IReadOnlyList<string> jokes)
         {
-            for(int i = 0; i < 3; i++)
-            {
-                Console.WriteLine("\n" + jokes[i] + "\n");
-            }
+            var rnd = new Random(); 
+
+            Console.WriteLine("\n" + jokes[rnd.Next(0, jokes.Count)] + "\n");
         }
     }
 }
