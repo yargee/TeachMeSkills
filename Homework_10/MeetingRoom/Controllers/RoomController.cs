5ddb7be025c4876c2cd6833289b7e8a5c90e38e5
yargee <yargee@bk.ru>
2023-11-02 14:36:38 +0300

    +

--- Homework_10/MeetingRoom/Controllers/RoomController.cs
+++ Homework_10/MeetingRoom/Controllers/RoomController.cs
@@ -4,46 +4,38 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MeetingRoom.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]")]  //что это значит и в чем котличие от mvc controller
     [ApiController]
     public class RoomController : ControllerBase
     {
         private IRoomServices _services;
         
-        public RoomController(IRoomServices services)
+        public RoomController(IRoomServices services)  //эта инициализация происходит под капотом?
         {
             _services = services;
-        }
-
-        [HttpGet]
-        public IActionResult Get()
-        {
-            return new JsonResult("fckmbrn");
-        }
-
-        [HttpPost]
-        public IActionResult Post(Participier participier)
-        {
-            return Ok(participier);
-        }
-        /*
+        }        
         
         [HttpGet]
-        public IParticipier[] GetParticipiers()
+        public Participier[] GetParticipiers()
         {
             return _services.GetParticipiers();
         }
 
-        [HttpPost]
-        public void AddParticipier([FromBody] IParticipier participier)
-        {
-            _services.AddParticipier(participier);
-        }
 
-        [HttpPost]
-        public void RemoveParticipier(IParticipier participier)
+        [HttpPost]  //сваггер работает только если тут один метод с пост, если два, то сразу ошибка
+        public void RemoveParticipier(Participier participier)
         {
             _services.RemoveParticipier(participier);
-        }*/
+            //return Ok(); //что именно должно возвращаться? есть ли какой-то список рекомендуемых реализаций?
+            //должно ли вообще возвращаться?
+        }
+        
+        [HttpPost]
+        public IActionResult AddParticipier(Participier participier)
+        {
+            _services.AddParticipier(participier);
+            return Ok();
+        }
+        
     }
 }
