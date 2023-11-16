2f941120b561ede23d1bda2d2389b1211978c61e
Pashinov Petr <yargee@bk.ru>
2023-11-16 14:41:52 +0300

    2 part wip

---
+++ Homework_14(Async)/Product Inventory Project/Program.cs
@@ -0,0 +1,23 @@
+﻿using ProductInventoryProject;
+using Microsoft.Extensions.Configuration;
+using ProductInventoryProject.Products;
+
+
+var outputHandler = new OutputHandler();
+outputHandler.Initialize();
+
+var stock = new Stock();
+stock.InitShop();
+
+while (true)
+{
+    stock.ShowProducts();
+    stock.SelectProduct(out Product product, out int quantity);
+
+    var purchase = new StockHandler();
+    purchase.TakeFromStock(product, quantity);
+
+    var inventory = new Inventory();
+    var result = inventory.GetInventoryAsString();
+    outputHandler.PrintResult(result);
+}
