729d03ee9b2682892c62ab07689a57f1c493a1aa
Pashinov Petr <yargee@bk.ru>
2023-11-16 14:27:44 +0300

    1 part

---
+++ Homework_14(Async)/BigBigArray/ArrayHandler.cs
@@ -0,0 +1,65 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace BigBigArray
+{
+    internal class ArrayHandler
+    {
+        private int[] _array;
+        private int _threads;
+
+        public ArrayHandler(int[] array, int threads)
+        {
+            _array = array;
+            _threads = threads;
+        }
+
+        public async Task<int> SumAllAsync()
+        {
+            int sum = 0;
+            var tasks = new List<Task<int>>();
+
+            for (int i = 0; i < _threads; i++)
+            {
+                tasks.Add(SumAsync(i, _threads));
+            }
+
+
+            await Task.WhenAll(tasks);
+            for (int i = 0; i < _threads; i++)
+            {
+                sum += tasks[i].Result;
+            }
+
+            return sum;
+        }
+
+        private async Task<int> SumAsync(int startIndex, int delta)
+        {
+            var tempList = new List<int>();
+            var sum = 0;
+            var sw = new Stopwatch();
+
+            sw.Start();
+
+            for (int i = startIndex; i < _array.Length; i += delta)
+            {
+                tempList.Add(_array[i]);
+            }
+
+            await Task.Run(() =>
+            {
+            for (int i = 0; i < tempList.Count; i++)
+            {
+                sum += tempList[i];
+            }
+            });
+
+            sw.Stop();
+
+            Console.WriteLine($"Thread {startIndex + 1} completed in {sw.ElapsedMilliseconds} ms. Sum: {sum}");
+
+            return sum;
+        }
+    }
+}
---
+++ Homework_14(Async)/BigBigArray/Menu.cs
@@ -0,0 +1,32 @@
+﻿namespace BigBigArray
+{
+    internal class Menu
+    {
+        private int[] _array = new int[10_000_000];
+
+        public void InitArray()
+        {
+            var rnd = new Random();
+
+            for (int i = 0; i < _array.Length; i++)
+            {
+                _array[i] = rnd.Next(-100,101);
+            }
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("Введите количество потоков");
+            var input = Console.ReadLine();
+
+            if(int.TryParse(input, out var index))
+            {
+                var handler = new ArrayHandler(_array, index);
+
+                var sum = handler.SumAllAsync();
+
+                Console.WriteLine($"Total sum: {sum.Result}");
+            }
+        }
+    }
+}
